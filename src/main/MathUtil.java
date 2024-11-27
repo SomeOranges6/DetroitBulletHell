3d0676b210adf36ff420bbdb49a6bca573387fc6
gorbnibno <339646028@gotvdsb.ca>
2024-11-27 12:42:15 -0500

    Updated MathUtil
    Added a method to calculate the total distance travelled by an entity.

--- src/main/MathUtil.java
+++ src/main/MathUtil.java
@@ -1,13 +1,16 @@
 package main;
 
 public class MathUtil {
-
 	
 	/**Calculates the distance between two points
-	 * @return An array with  the distance on the x **/
-	public int[] distFrom(int[] startPoint, int[] endPoint) {
+	 * @return An array with the distance on the x **/
+	public int distFrom(int[] startPoint, int[] endPoint) {
 		int xDist = startPoint[0] - endPoint[0];
 		int yDist = startPoint[1] - endPoint[1];
 		
+		double totalDist = Math.hypot(xDist, yDist); 
+		return (int)totalDist;
 	}
+	
+	
 }
