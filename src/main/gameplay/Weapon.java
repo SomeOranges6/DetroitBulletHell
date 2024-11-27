f7cc6c73ff5f210cbe44dcc19224ce93e2c20852
SomeOranges6 <363374786@gotvdsb.ca>
2024-11-27 17:25:42 -0500

    Small edit to the weapon class

--- src/main/gameplay/Weapon.java
+++ src/main/gameplay/Weapon.java
@@ -1,4 +1,11 @@
 package main.gameplay;
 
-public class Weapon {
+import java.awt.image.BufferedImage;
+
+public record Weapon(int damage, int firerate, BufferedImage texture) {
+
+    /** Will typically spawn a projectile when shot **/
+    public void onShoot(){
+
+    }
 }
