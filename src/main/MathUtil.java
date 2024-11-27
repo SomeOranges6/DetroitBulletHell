427f8b54b967e6d2bbcf656140046bf273fa4a29
gorbnibno <339646028@gotvdsb.ca>
2024-11-27 12:53:24 -0500

    Update MathUtil.java
    Changed distFrom to getDistFrom: it now returns an array of two points rather than the hypotenuse of them.

--- src/main/MathUtil.java
+++ src/main/MathUtil.java
@@ -4,12 +4,12 @@ public class MathUtil {
 	
 	/**Calculates the distance between two points
 	 * @return An array with the distance on the x **/
-	public int distFrom(int[] startPoint, int[] endPoint) {
+	public int[] getDistFrom(int[] startPoint, int[] endPoint) {
 		int xDist = startPoint[0] - endPoint[0];
 		int yDist = startPoint[1] - endPoint[1];
 		
-		double totalDist = Math.hypot(xDist, yDist); 
-		return (int)totalDist;
+		int[] distArray = new int[1];
+		return distArray;
 	}
 	
 	
