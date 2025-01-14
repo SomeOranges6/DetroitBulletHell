a679cd96f552ec1e4e13c2399eaf6d0c06e4faa8
gorbnibno <339646028@gotvdsb.ca>
2025-01-14 11:54:29 -0500

    Update HowToPlay.java
    Tweaked the text strings so they actually show up now.

--- src/main/HowToPlay.java
+++ src/main/HowToPlay.java
@@ -59,12 +59,13 @@ public class HowToPlay {
 			Graphics2D g2d = (Graphics2D)g;
 			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 			
-			Font tutorialFont = new Font("Arial", Font.BOLD, 20);
+			g2d.setColor(Color.RED);
+			Font tutorialFont = new Font("Arial", Font.BOLD, 40);
             g2d.setFont(tutorialFont);
             
-            g2d.drawString("Press U to Attack and Start Game", ALLBITS, ABORT);
-            g2d.drawString("Press I to Switch Weapons", ALLBITS, ABORT);
-            g2d.drawString("Press J to Pause", ALLBITS, ABORT);
+            g2d.drawString("Press U to Attack and Start Game", 20, 100);
+            g2d.drawString("Press I to Switch Weapons", 20, 300);
+            g2d.drawString("Press J to Pause", 20, 500);
 		}
 	}
 }
